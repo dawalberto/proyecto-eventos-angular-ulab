d77d1fb1bc3dcf61439ef16d0cb138090e10a535
Alberto Garcia <albertoinformatica97@gmail.com>
2019-08-08 14:06:56 +0200

    Filtrar eventos terminado

--- src/app/events-show/events-show.component.ts
+++ src/app/events-show/events-show.component.ts
@@ -16,18 +16,20 @@ export class EventsShowComponent implements OnInit {
   events: IEvent[] = [
     {
       title: 'Fiesta del comic',
-      image: 'https://partymax.co/wp-content/uploads/2019/03/71Drhnh-IYL._SL1000_.jpg',
+      image: 'assets/img1.jpg',
       date: new Date(2019, 10, 9).toDateString(),
       description: 'Fiesta para todos los fans de los comics',
       price: 9
     },
     {
       title: 'Encuentro de malabares',
-      image: 'https://cdn.juegosmalabares.com/blog/wp-content/uploads/2014/03/the-pastels-ganadores-equipos-ija-2013.jpg',
+      image: 'assets/img2.jpg',
       date: new Date(2019, 11, 15).toDateString(),
       description: 'Encuentro de malabaristas y artes cirquenses',
       price: 7
     }
   ]
+  filterSearch: string = ''
+
 
 }
